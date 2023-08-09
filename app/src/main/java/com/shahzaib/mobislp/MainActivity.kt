1bed9a7a21c00db5d1fa829343f7bb9677106763
Shahzaib Waseem <shahzaib.waseem96@gmail.com>
2023-08-09 16:55:11 -0700

    initial working project upload

---
+++ app/src/main/java/com/shahzaib/mobislp/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.shahzaib.mobislp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Rect
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.shahzaib.mobislp.databinding.ActivityMainBinding
+
+class MainActivity: AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+        makeDirectory(Utils.rawImageDirectory)
+        makeDirectory(Utils.croppedImageDirectory)
+        makeDirectory(Utils.processedImageDirectory)
+        makeDirectory(Utils.hypercubeDirectory)
+    }
+
+    companion object {
+        const val MOBISPECTRAL_APPLICATION = 0
+        lateinit var fruitID: String
+        lateinit var originalRGBBitmap: Bitmap
+        lateinit var originalNIRBitmap: Bitmap
+        lateinit var originalImageRGB: String
+        lateinit var originalImageNIR: String
+        lateinit var processedImageRGB: String
+        lateinit var processedImageNIR: String
+        var croppedImageRGB: String = ""
+        var croppedImageNIR: String = ""
+        var actualLabel: String = ""
+        var predictedLabel: String = ""
+        var normalizationTime: String = " s"
+        var reconstructionTime: String = " s"
+        var classificationTime: String = " ms"
+        lateinit var tempRGBBitmap: Bitmap
+        lateinit var tempRectangle: Rect
+
+        fun generateAlertBox(context: Context, title: String, text: String, onPositiveButtonFunction: (() -> Unit)) {
+            val alertDialogBuilder = MaterialAlertDialogBuilder(context, R.style.AlertDialogTheme)
+            alertDialogBuilder.setMessage(text)
+            alertDialogBuilder.setTitle(title)
+            alertDialogBuilder.setCancelable(false)
+            if (title == "Information")
+                alertDialogBuilder.setPositiveButton("Okay") { dialog, _ -> dialog?.cancel() }
+            else
+                alertDialogBuilder.setPositiveButton("Reload") { _, _ -> onPositiveButtonFunction() }
+
+            val alertDialog = alertDialogBuilder.create()
+            alertDialog.show()
+        }
+    }
+}
---
+++ app/src/main/java/com/shahzaib/mobislp/fragments/ApplicationSelectorFragment.kt
@@ -0,0 +1,119 @@
+package com.shahzaib.mobislp.fragments
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.ImageFormat
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.withStarted
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+import com.shahzaib.mobislp.MainActivity
+import com.shahzaib.mobislp.MainActivity.Companion.generateAlertBox
+import com.shahzaib.mobislp.R
+import com.shahzaib.mobislp.Utils
+import com.shahzaib.mobislp.databinding.FragmentApplicationselectorBinding
+import com.shahzaib.mobislp.makeDirectory
+import kotlinx.coroutines.launch
+
+class ApplicationSelectorFragment: Fragment() {
+    private lateinit var fragmentApplicationselectorBinding: FragmentApplicationselectorBinding
+    private lateinit var applicationArray: Array<String>
+
+    /** Host's navigation controller */
+    private val navController: NavController by lazy {
+        Navigation.findNavController(requireActivity(), R.id.fragment_container)
+    }
+
+    private fun NavController.safeNavigate(direction: NavDirections) {
+        currentDestination?.getAction(direction.actionId)?.run {
+            navigate(direction)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        fragmentApplicationselectorBinding = FragmentApplicationselectorBinding.inflate(inflater, container, false)
+        val applicationPicker = fragmentApplicationselectorBinding.applicationPicker
+        applicationArray = arrayOf(getString(R.string.avocado_string),
+            getString(R.string.pear_string) , getString(R.string.apple_string))
+        applicationPicker.minValue = 0
+        applicationPicker.maxValue = applicationArray.size-1
+        applicationPicker.displayedValues = applicationArray
+
+        makeDirectory(Utils.rawImageDirectory)
+        makeDirectory(Utils.croppedImageDirectory)
+        makeDirectory(Utils.processedImageDirectory)
+        makeDirectory(Utils.hypercubeDirectory)
+
+        return fragmentApplicationselectorBinding.root
+    }
+
+    private fun disableButton(cameraIdNIR: String) {
+        if (cameraIdNIR == "No NIR Camera") {
+            fragmentApplicationselectorBinding.runApplicationButton.isEnabled = false
+            fragmentApplicationselectorBinding.runApplicationButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.sfu_dark_gray))
+            fragmentApplicationselectorBinding.runApplicationButton.text = resources.getString(R.string.no_nir_warning)
+            fragmentApplicationselectorBinding.runApplicationButton.transformationMethod = null
+        }
+    }
+
+    private fun enableButton() {
+        fragmentApplicationselectorBinding.runApplicationButton.isEnabled = true
+        fragmentApplicationselectorBinding.runApplicationButton.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.sfu_primary))
+        fragmentApplicationselectorBinding.runApplicationButton.text = resources.getString(R.string.launch_application_button).uppercase()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onStart() {
+        super.onStart()
+        val cameraIdNIR = Utils.getCameraIDs(requireContext(), MainActivity.MOBISPECTRAL_APPLICATION).second
+
+        fragmentApplicationselectorBinding.information.setOnClickListener {
+            generateAlertBox(requireContext(), "Information", getString(R.string.application_selector_information_string)) { }
+        }
+
+        fragmentApplicationselectorBinding.radioGroup.setOnCheckedChangeListener { _, _ ->
+            val selectedRadio = fragmentApplicationselectorBinding.radioGroup.checkedRadioButtonId
+            val selectedOption = requireView().findViewById<RadioButton>(selectedRadio).text.toString()
+            if (selectedOption == getString(R.string.offline_mode_string))
+                enableButton()
+            else
+                disableButton(cameraIdNIR)
+        }
+
+        if (cameraIdNIR == "No NIR Camera"){
+            fragmentApplicationselectorBinding.onlineMode.isEnabled = false
+        }
+
+        fragmentApplicationselectorBinding.runApplicationButton.setOnTouchListener { _, _ ->
+            val selectedApplication = applicationArray[fragmentApplicationselectorBinding.applicationPicker.value]
+            val selectedRadio = fragmentApplicationselectorBinding.radioGroup.checkedRadioButtonId
+            val selectedOption = requireView().findViewById<RadioButton>(selectedRadio).text.toString()
+            val offlineMode = selectedOption == getString(R.string.offline_mode_string)
+            val sharedPreferences = requireActivity().getSharedPreferences("mobislp_preferences", Context.MODE_PRIVATE)
+            val editor = sharedPreferences?.edit()
+            editor!!.putString("application", selectedApplication)
+            editor.putString("option", getString(R.string.advanced_option_string))
+            editor.putBoolean("offline_mode", offlineMode)
+            Log.i("Radio Button", "$selectedApplication, $selectedOption")
+            editor.apply()
+//            lifecycleScope.launch {
+//                withStarted {
+//                    navController.safeNavigate(ApplicationSelectorFragmentDirections.actionAppselectorToCameraFragment(
+//                        Utils.getCameraIDs(requireContext(), MainActivity.MOBISPECTRAL_APPLICATION).first, ImageFormat.JPEG)
+//                    )
+//                }
+//            }
+
+            true
+        }
+    }
+}
